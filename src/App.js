5c8373be42528873e525adc74c180c75b67273a7
liamskydamien <liamskydamien@gmil.com>
2022-12-23 19:15:26 +0100

    Adding grid

--- src/App.js
+++ src/App.js
@@ -12,21 +12,49 @@ function App() {
                     <h1>Title</h1>
                 </div>
                 <div className="div3">
-                    <h1>Card 1</h1>
+                    <h1>Experience</h1>
                 </div>
                 <div className="div4">
-                    <h1>Card 1</h1>
+                    <h1>Education</h1>
                 </div>
                 <div className="div5">
-                    <h1>Card 1</h1>
+                    <h1>Languages</h1>
                 </div>
                 <div className="div6">
-                    <h1>Card 1</h1>
+                    <h1>Interests</h1>
                 </div>
                 <div className="div7">
                     <h1>Card 1</h1>
                 </div>
-
+                <div className="div8">
+                    <h1>Recent Projects</h1>
+                </div>
+            </div>
+        <div className="skills-grid">
+            <div className="div1">
+                <h1>Skills</h1>
+            </div>
+            <div className="div2">
+                <h1>Title</h1>
+            </div>
+            <div className="div3">
+                <h1>Experience</h1>
+            </div>
+            <div className="div4">
+                <h1>Education</h1>
+            </div>
+            <div className="div5">
+                <h1>Languages</h1>
+            </div>
+            <div className="div6">
+                <h1>Interests</h1>
+            </div>
+            <div className="div7">
+                <h1>Card 1</h1>
+            </div>
+            <div className="div8">
+                <h1>Recent Projects</h1>
+            </div>
         </div>
     </div>
   );
