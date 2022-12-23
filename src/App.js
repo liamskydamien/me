0f58c4d600ab3c759cb1138ffc172b8aadd643d2
liamskydamien <liamskydamien@gmil.com>
2022-12-23 18:59:39 +0100

    Adding grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from "./UI/Card/Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <div className="parent">
+                <div className="div1">
+                </div>
+                <div className="div2">
+                    <h1>Title</h1>
+                </div>
+                <div className="div3">
+                    <h1>Card 1</h1>
+                </div>
+                <div className="div4">
+                    <h1>Card 1</h1>
+                </div>
+                <div className="div5">
+                    <h1>Card 1</h1>
+                </div>
+                <div className="div6">
+                    <h1>Card 1</h1>
+                </div>
+                <div className="div7">
+                    <h1>Card 1</h1>
+                </div>
+
+        </div>
     </div>
   );
 }
--- src/UI/Card/Card.js
+++ src/UI/Card/Card.js
@@ -1,6 +1,6 @@
 import classes from './Card.module.css';
 const Card = props => {
-    return  <div className={classes.card}>
+    return  <div className={`${classes.card} ${props.className}`}>
                 {props.children}
             </div>
 }
