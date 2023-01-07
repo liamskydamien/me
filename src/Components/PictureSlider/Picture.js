c7f338dd7f336474ef9b6a51a3e74c2c96b5c570
Liam Hess <liamskydamien@gmail.com>
2023-01-07 18:36:41 +0100

    Fixing images

--- src/Components/PictureSlider/Picture.js
+++ src/Components/PictureSlider/Picture.js
@@ -1,13 +1,16 @@
 import styles from "./Picture.module.css";
+import pic1 from "../../Pictures/Unbenannt-1.png";
+import pic2 from "../../Pictures/Unbenannt-2.png";
+import pic3 from "../../Pictures/Unbenannt-3.png";
 
 const Picture = (props) => {
     const images = [
-        styles.first,
-        styles.second,
-        styles.third
+        pic1,
+        pic2,
+        pic3
     ];
 
     const classes = props.className + " " + styles.backGround + " " + images[props.src];
-    return  <div className={classes}/>
+    return  <img src={images[props.src]} alt={"Ein Bild von mir"} className={classes}/>
 }
 export default Picture;
