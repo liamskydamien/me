ea5c5057a93fa1740d5ab060f8214c464bea3b06
Liam Hess <liamskydamien@gmail.com>
2023-01-08 22:56:59 +0100

    Adding darkmode toggle

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,29 +1,41 @@
 import styles from './Navbar.module.css'
 import {FaEnvelope, FaLinkedinIn, FaGithub, FaInstagram} from "react-icons/fa";
-const Navbar = () => {
-    return <div className={styles.Navbar}>
-        <div className="Navbar__Logo">
-            <a href="/">Logo</a>
-        </div>
-        <div className="Navbar__Links">
-            <a href="/">Home</a>
-            <a href="/">About</a>
-            <a href="/">Contact</a>
-        </div>
-        <div className={styles.socials}>
-            <a className={styles.Email} href="/">
-                <FaEnvelope size={35}/>
-            </a>
-            <a className={styles.LinkedIn} href="/">
-                <FaLinkedinIn size={35}/>
-            </a>
-            <a className={styles.GitHub} href="/">
-                <FaGithub size={35}/>
-            </a>
-            <a className={styles.Instagram} href="/">
-                <FaInstagram size={35}/>
-            </a>
-        </div>
-    </div>
+import {useState} from "react";
+import ReactSwitch from "react-switch";
+const Navbar = props => {
+    const theme = props.theme;
+    const toggleTheme = () => {
+        props.onThemeChange();
+        setChecked(!checked);
+    }
+    const [checked, setChecked] = useState(false);
+
+    return  <div className={styles.Navbar + " " + theme}>
+                <div className="Navbar__Logo">
+                    <a href="/">Logo</a>
+                </div>
+                <div className={styles.navbarLinks}>
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contact</a>
+                </div>
+                <div className={styles.socials}>
+                    <a className={styles.Email} href="/">
+                        <FaEnvelope size={35}/>
+                    </a>
+                    <a className={styles.LinkedIn} href="/">
+                        <FaLinkedinIn size={35}/>
+                    </a>
+                    <a className={styles.GitHub} href="/">
+                        <FaGithub size={35}/>
+                    </a>
+                    <a className={styles.Instagram} href="/">
+                        <FaInstagram size={35}/>
+                    </a>
+                    <div className={styles.themeSwitch}>
+                        <ReactSwitch checked={checked} onChange={toggleTheme}></ReactSwitch>
+                    </div>
+                </div>
+            </div>
 }
 export default Navbar;
