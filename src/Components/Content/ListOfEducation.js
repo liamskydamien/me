7f0d85fd104450debaab9f91c3faff656c2fc6c8
Liam Hess <liamskydamien@gmail.com>
2023-01-14 20:54:21 +0100

    Styling

--- src/Components/Content/ListOfEducation.js
+++ src/Components/Content/ListOfEducation.js
@@ -6,17 +6,21 @@ const ListOfEducation = () => {
         <h3>Education</h3>
             <div className={styles.wrapper}>
                 <img src={hbrs} alt="Logo der Hochschule Bonn-Rhein-Sieg"/>
-                <h4>Hochschule Bonn-Rhein-Sieg</h4>
-                <h5>Bachelor of Science Wirtschaftsinformatik</h5>
-                <p>Derzeit im 3. Fachsemester voraussichtlicher Abschluss 2024.
-                    Aktueller Notendurchschnitt: 1.7
-                </p>
+                <div className={styles.content}>
+                    <h4>Hochschule Bonn-Rhein-Sieg</h4>
+                    <h5>Bachelor of Science Wirtschaftsinformatik</h5>
+                    <p>Derzeit im 3. Fachsemester voraussichtlicher Abschluss 10/2024.
+                        Aktueller Notendurchschnitt: 1.7
+                    </p>
+                </div>
             </div>
         <div className={styles.wrapper}>
             <img src={hbrs} alt="Logo der Hochschule Bonn-Rhein-Sieg"/>
-            <h4>Gymnasium am Ölberg</h4>
-            <h5>Allgemeine Hochschulreife</h5>
-            <p>Abschluss am 06/2020 mit einem Notendurchschnitt von 2.0</p>
+            <div className={styles.content}>
+                <h4>Gymnasium am Ölberg</h4>
+                <h5>Allgemeine Hochschulreife</h5>
+                <p>Abschluss am 06/2020 mit einem Notendurchschnitt von 2.0</p>
+            </div>
         </div>
     </React.Fragment>
 }
