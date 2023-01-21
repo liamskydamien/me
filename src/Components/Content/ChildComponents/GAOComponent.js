893a1848d933e105dac192ae43e3a93e1ab9ac5a
Liam Hess <liamskydamien@gmail.com>
2023-01-21 19:54:34 +0100

    Adding GaO

--- src/Components/Content/ChildComponents/GAOComponent.js
+++ src/Components/Content/ChildComponents/GAOComponent.js
@@ -1,15 +1,31 @@
 import styles from "./componentStyles.module.css";
 import gao from "../../../Pictures/GaO.png";
-import React from "react";
+import React, {useState} from "react";
+import specificStyles from "./gaoStyles.module.css";
+import {FaSquareRootAlt, FaCity, FaCode, FaRegComments, FaSquare} from "react-icons/fa";
 
 const GAOComponent = () => {
-    return <div className={styles.wrapper}>
+    const [isExpanded, setIsExpanded] = useState(specificStyles.hidden);
+    const expansionHandler = () => {
+        if (isExpanded === specificStyles.hidden) {
+            setIsExpanded(specificStyles.active);
+        } else {
+            setIsExpanded(specificStyles.hidden);
+        }
+    };
+    return <div className={styles.wrapper} onClick={expansionHandler}>
         <img src={gao} alt="Logo der Hochschule Bonn-Rhein-Sieg"/>
         <div className={styles.content}>
             <h4>Gymnasium am Ölberg</h4>
             <h5>Allgemeine Hochschulreife</h5>
             <p>Abschluss am 06/2020 mit einem
                 Notendurchschnitt von 2.0</p>
+        </div>
+        <div className={styles.skills + " " + isExpanded + " " + specificStyles.skills}>
+            <div className={styles.skill}><FaSquareRootAlt/> Mathematics</div>
+            <div className={styles.skill}><FaCity/> Macro-Economics and Politics</div>
+            <div className={styles.skill}><FaCode/> Informatics</div>
+            <div className={styles.skill}><FaRegComments/> English</div>
         </div>
     </div>
 }
