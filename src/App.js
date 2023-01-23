6cd0d4b01ae0ae10da2654231b9873182e4fb86d
Liam Hess <liamskydamien@gmail.com>
2023-01-23 19:59:48 +0100

    Layout

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import ListOfEducation from "./Components/Content/ListOfEducation";
 import ListofWork from "./Components/Content/ListofWork";
 import Skills from "./Components/Skills/Skills";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
     const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -17,44 +18,46 @@ function App() {
     return  <div className="App" data-theme={theme}>
                 <Navbar onThemeChange={toggleThemeHandler}></Navbar>
                 <div className="body">
-                  <div className="aboutMe">
-                      <Card className="Header">
-                          <h2>About me</h2>
-                      </Card>
-                      <Slideshow className="PictureSlider"/>
-                      <Card className="Hi">
-                          <h2 style={{textAlign:"left"}}>Hi, I'm Liam</h2>
-                      </Card>
-                      <Card className="General">
-                          <p style={{textAlign:"left"}} >Liam Sky Damien Heß</p>
-                          <p style={{textAlign:"left"}} >  Born in Bonn, Germany on the 15th of August 2001</p>
-                          <p style={{textAlign:"left"}}> Address: Alter Heeresweg 11, 53639 Königswinter</p>
-                      </Card>
-                      <Card className="AboutMyself">
-                          <p>
-                              My name is Liam and I'm 21 years old. I'm a student at the HBRS in Bonn and I'm currently studying computer science.
-                          </p>
-                      </Card>
-                  </div>
-                  <div className="experiences">
-                      <Card className="ExperiencesHeader">
-                          <h2>Experiences</h2>
-                      </Card>
-                      <Card className="Work">
-                          <ListofWork theme={theme}/>
-                      </Card>
-                      <Card className="Education expandableCard">
-                          <ListOfEducation/>
-                      </Card>
-                  </div>
+                <div className="width">
+                    <div className="aboutMe">
+                        <Card className="Header">
+                            <h2>About me</h2>
+                        </Card>
+                        <Slideshow className="PictureSlider"/>
+                        <Card className="Hi">
+                            <h2 style={{textAlign:"left"}}>Hi, I'm Liam</h2>
+                        </Card>
+                        <Card className="General">
+                            <p style={{textAlign:"left"}} >Liam Sky Damien Heß</p>
+                            <p style={{textAlign:"left"}} >  Born in Bonn, Germany on the 15th of August 2001</p>
+                            <p style={{textAlign:"left"}}> Address: Alter Heeresweg 11, 53639 Königswinter</p>
+                        </Card>
+                        <Card className="AboutMyself">
+                            <p>
+                                My name is Liam and I'm 21 years old. I'm a student at the HBRS in Bonn and I'm currently studying computer science.
+                            </p>
+                        </Card>
+                    </div>
+                    <div className="experiences">
+                        <Card className="ExperiencesHeader">
+                            <h2>Experiences</h2>
+                        </Card>
+                        <Card className="Work">
+                            <ListofWork theme={theme}/>
+                        </Card>
+                        <Card className="Education expandableCard">
+                            <ListOfEducation/>
+                        </Card>
+                    </div>
                     <div className="Skills">
                         <Skills/>
                     </div>
                     <div className="Projects">
-
                     </div>
                 </div>
 
+                    <Footer className="footer"/>
+                </div>
             </div>
 }
 export default App;
