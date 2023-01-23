e0cf869f6786c9776378468dd7e8e3a2a40dc56a
Liam Hess <liamskydamien@gmail.com>
2023-01-23 19:39:06 +0100

    Layout

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,14 +1,28 @@
 import Card from "../../UI/Card/Card";
 import styles from "./Skills.module.css";
 const Skills = () => {
-    return <div>
-        <Card className={styles.header}/>
-        <Card className={styles.programming}/>
-        <Card className={styles.design}/>
-        <Card className={styles.languages}/>
-        <Card className={styles.hobbies}/>
-        <Card className={styles.communication}/>
-        <Card className={styles.management}/>
+    return <div className={styles.layout}>
+        <Card className={styles.header}>
+            <h2>Skills</h2>
+        </Card>
+        <Card className={styles.programming}>
+            <h3>Programming</h3>
+        </Card>
+        <Card className={styles.design}>
+            <h3>Design</h3>
+        </Card>
+        <Card className={styles.languages}>
+            <h3>Language</h3>
+        </Card>
+        <Card className={styles.hobbies}>
+            <h3>Hobbies</h3>
+        </Card>
+        <Card className={styles.communication}>
+            <h3>Communication</h3>
+        </Card>
+        <Card className={styles.management}>
+            <h3>Management</h3>
+        </Card>
     </div>
 }
 export default Skills;
