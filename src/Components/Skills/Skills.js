eb811ff3180e051875c63244fd125116ef59b2a3
Liam Hess <liamskydamien@gmail.com>
2023-01-27 00:35:45 +0100

    Chaning some styles

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,27 +1,29 @@
 import Card from "../../UI/Card/Card";
 import styles from "./Skills.module.css";
-import SkillCard from "./SkillCard";
-import {FaArchive} from "react-icons/fa";
 const Skills = () => {
+    const card = styles.card;
     return <div className={styles.layout}>
         <Card className={styles.header}>
             <h2>Skills</h2>
         </Card>
-        <div className={styles.design}>
+        <Card className={styles.programming}>
+            <h3>Programming</h3>
+        </Card>
+        <Card className={styles.design}>
             <h3>Design</h3>
-        </div>
-        <div className={styles.languages}>
+        </Card>
+        <Card className={styles.languages}>
             <h3>Language</h3>
-        </div>
-        <div className={styles.hobbies}>
+        </Card>
+        <Card className={styles.hobbies}>
             <h3>Hobbies</h3>
-        </div>
-        <div className={styles.communication}>
+        </Card>
+        <Card className={styles.communication}>
             <h3>Communication</h3>
-        </div>
-        <div className={styles.management}>
+        </Card>
+        <Card className={styles.management}>
             <h3>Management</h3>
-        </div>
+        </Card>
     </div>
 }
 export default Skills;
