8e22e0e104ec9e9e776f3c159d93d5344bfe06d0
Liam Hess <liamskydamien@gmail.com>
2023-01-27 00:32:34 +0100

    Chaning some styles

--- src/Components/Skills/IconSkill.js
+++
@@ -1,8 +0,0 @@
-import styles from './IconSkill.module.css';
-const IconSkill = props =>{
-    return <div className={styles.wrapper}>
-        {props.icon}
-        <p>{props.name}</p>
-    </div>
-};
-export default IconSkill;
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -3,53 +3,25 @@ import styles from "./Skills.module.css";
 import SkillCard from "./SkillCard";
 import {FaArchive} from "react-icons/fa";
 const Skills = () => {
-    const skills = [
-        {
-            id: 1,
-            title: "Design",
-            icon: <FaArchive/>
-        },
-        {
-            id: 2,
-            title: "Languages",
-            icon: <FaArchive/>,
-        },
-        {
-            id: 3,
-            title: "Hobbies",
-            icon: <FaArchive/>
-        },
-        {
-            id: 4,
-            title: "Communication",
-            icon: <FaArchive/>,
-        },
-        {
-            id: 5,
-            title: "Management",
-            icon: <FaArchive/>,
-        }
-    ]
     return <div className={styles.layout}>
         <Card className={styles.header}>
             <h2>Skills</h2>
         </Card>
-        <SkillCard items={skills}/>
-        <Card className={styles.design}>
+        <div className={styles.design}>
             <h3>Design</h3>
-        </Card>
-        <Card className={styles.languages}>
+        </div>
+        <div className={styles.languages}>
             <h3>Language</h3>
-        </Card>
-        <Card className={styles.hobbies}>
+        </div>
+        <div className={styles.hobbies}>
             <h3>Hobbies</h3>
-        </Card>
-        <Card className={styles.communication}>
+        </div>
+        <div className={styles.communication}>
             <h3>Communication</h3>
-        </Card>
-        <Card className={styles.management}>
+        </div>
+        <div className={styles.management}>
             <h3>Management</h3>
-        </Card>
+        </div>
     </div>
 }
 export default Skills;
