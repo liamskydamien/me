2b769650cc635915aaacc95f888272803f5b7079
Liam Hess <liamskydamien@gmail.com>
2023-01-29 17:41:57 +0100

    Finishing Skills

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,8 +1,25 @@
 import Card from "../../UI/Card/Card";
 import styles from "./Skills.module.css";
-import {FaCode, FaCss3, FaCss3Alt, FaFigma, FaFlag, FaHtml5, FaJava, FaJs, FaReact} from "react-icons/fa";
+import {
+    FaAtlassian, FaBusinessTime, FaCamera,
+    FaCode,
+    FaCss3Alt,
+    FaFigma,
+    FaFlag, FaGlobe,
+    FaHtml5,
+    FaJava, FaJira,
+    FaJs, FaLightbulb,
+    FaReact, FaSprayCan,
+} from "react-icons/fa";
 import Skill from "./Skill";
-import {SiAdobexd, SiAdobephotoshop, SiWordpress} from "react-icons/si";
+import {
+    SiAdobexd,
+    SiAdobephotoshop,
+    SiWordpress,
+    SiMicrosoftpowerpoint,
+    SiMicrosoftexcel, SiMicrosoftword
+} from "react-icons/si";
+import {MdSurfing} from "react-icons/md";
 const Skills = () => {
     const card = styles.card;
     return <div className={styles.layout}>
@@ -32,28 +49,34 @@ const Skills = () => {
             <h3>Languages</h3>
             <div className={styles.list}>
                 <Skill color={'blue'} icon={<FaFlag size={20}/>} title={"German - Native Speaker"}/>
-                <Skill color={'green'} icon={<FaJs size={20}/>} title={"English - Fluent"}/>
+                <Skill color={'green'} icon={<FaFlag size={20}/>} title={"English - Fluent"}/>
             </div>
         </Card>
         <Card className={styles.hobbies}>
-            <h3>Hobbies</h3>
+            <h3>Interests</h3>
             <div className={styles.list}>
-                <Skill color={'blue'} icon={<FaFlag size={20}/>} title={"German - Native Speaker"}/>
-                <Skill color={'green'} icon={<FaJs size={20}/>} title={"English - Fluent"}/>
+                <Skill color={'darkPurple'} icon={<FaCode size={20}/>} title={"Programming"}/>
+                <Skill color={'green'} icon={<FaCamera size={20}/>} title={"Photography"}/>
+                <Skill color={'orange'} icon={<FaGlobe size={20}/>} title={"Traveling"}/>
+                <Skill color={'darkBlue'} icon={<MdSurfing size={20}/>} title={"Surfing"}/>
+                <Skill color={'darkGreen'} icon={<FaBusinessTime size={20}/>} title={"Entrepreneurship"}/>
+                <Skill color={'blue'} icon={<FaSprayCan size={20}/>} title={"Street-Art"}/>
             </div>
         </Card>
         <Card className={styles.communication}>
             <h3>Office</h3>
             <div className={styles.list}>
-                <Skill color={'blue'} icon={<FaFlag size={20}/>} title={"German - Native Speaker"}/>
-                <Skill color={'green'} icon={<FaJs size={20}/>} title={"English - Fluent"}/>
+                <Skill color={'darkGreen'} icon={<SiMicrosoftexcel size={20}/>} title={"Microsoft Excel"}/>
+                <Skill color={'orange'} icon={<SiMicrosoftpowerpoint size={20}/>} title={"Microsoft PowerPoint"}/>
+                <Skill color={'darkBlue'} icon={<SiMicrosoftword size={20}/>} title={"Microsoft Word"}/>
             </div>
         </Card>
         <Card className={styles.management}>
-            <h3>Soft-Skills</h3>
+            <h3>Soft-Skills / Management</h3>
             <div className={styles.list}>
-                <Skill color={'blue'} icon={<FaFlag size={20}/>} title={"SCRUM"}/>
-                <Skill color={'green'} icon={<FaJs size={20}/>} title={"English - Fluent"}/>
+                <Skill color={'blue'} icon={<FaAtlassian size={20}/>} title={"Confluence"}/>
+                <Skill color={'darkblue'} icon={<FaJira size={20}/>} title={"JIRA"}/>
+                <Skill color={'purple'} icon={<FaLightbulb size={20}/>} title={"Design Thinking"}/>
             </div>
         </Card>
     </div>
