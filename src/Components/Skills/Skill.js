eb20bc26aaa96c3f84cc05f1ac4a61e418582d7c
Liam Hess <liamskydamien@gmail.com>
2023-01-29 16:54:28 +0100

    Adding switch for deciding color

--- src/Components/Skills/Skill.js
+++ src/Components/Skills/Skill.js
@@ -1,6 +1,29 @@
 import styles from './Skill.module.css'
 const Skill = props => {
-    const classes = styles.wrapper + ` styles.${props.color}`;
+    const color = () => {
+        switch (props.color) {
+            case 'red':
+                return styles.red;
+            case 'green':
+                return styles.green;
+            case 'yellow':
+                return styles.yellow;
+            case 'blue':
+                return styles.blue;
+            case 'darkGreen':
+                return styles.darkGreen;
+            case 'purple':
+                return styles.purple;
+            case 'darkPurple':
+                return styles.darkPurple;
+            case 'orange':
+                return styles.orange;
+            default:
+                return styles.darkBlue;
+        }
+    }
+
+    const classes = styles.wrapper + ' ' + color();
     return <div className={classes}>
         <div>{props.icon}</div>
         <div>{props.title}</div>
