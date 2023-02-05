09f3f3493473fd89e72e44788f4182afae85ad02
Liam Hess <liamskydamien@gmail.com>
2023-02-05 17:04:19 +0100

    Finishing Touch

--- src/Components/Projects/ProjectsCarousel.js
+++ src/Components/Projects/ProjectsCarousel.js
@@ -3,6 +3,7 @@ import ProjectItem from "./ProjectItem";
 import classes from "./ProjectsCarousel.module.css";
 import IWJunior from "../../Pictures/IWJunior.png";
 import BJS from "../../Pictures/BJS.png";
+import ReactLogo from "../../Pictures/React.png";
 const ProjectsCarousel = () =>{
 
     return <Card className={classes.background}>
@@ -12,16 +13,15 @@ const ProjectsCarousel = () =>{
                          description={"During a project at the university, " +
                              "I worked together with other students on creating innovative ideas to make the IW-Junior student cup better. My main task was to create a App which is an interactive component in the cup."}
                          link={"https://www.figma.com/file/6iTPYaq6zxYW7HCb6fnVI2/DesignThinking?node-id=0%3A1&t=aQAJTjsYyXfNWker-1"}/>
-            <ProjectItem img={IWJunior}
-                         title={"App Design IW-Junior"}
-                         description={"During a project at the university, " +
-                             "I worked together with other students on creating innovative ideas to make the IW-Junior student cup better. My main task was to create a App which is an interactive component in the cup."}
-                         link={"https://www.figma.com/file/6iTPYaq6zxYW7HCb6fnVI2/DesignThinking?node-id=0%3A1&t=aQAJTjsYyXfNWker-1"}/>
-            <ProjectItem img={IWJunior}
-                         title={"App Design IW-Junior"}
-                         description={"During a project at the university, " +
-                             "I worked together with other students on creating innovative ideas to make the IW-Junior student cup better. My main task was to create a App which is an interactive component in the cup."}
-                         link={"https://www.figma.com/file/6iTPYaq6zxYW7HCb6fnVI2/DesignThinking?node-id=0%3A1&t=aQAJTjsYyXfNWker-1"}/>
+            <ProjectItem img={BJS}
+                         title={"API and Web App for BJS"}
+                         description={"As my final project at my school, I created with 2 other students a Full-Stack Web App for the BJS." +
+                             "The BJS App is for managing Sportresults made during the Bundesjugendspiele (BJS). With the App you can create, edit and delete Sportresults. Aswell as calculating the pints for each student."}
+                         link={"https://github.com/bjs-org"}/>
+            <ProjectItem img={ReactLogo}
+                         title={"This Website"}
+                         description={"This Website is my first bigger React Project. I created it to showcase my CV and projects. More projects will be added in the future."}
+                         link={"https://liamskydamien.github.io/me/"}/>
         </div>
     </Card>;
 
