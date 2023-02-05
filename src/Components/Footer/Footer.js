532e04b6b9d886322369e9fde8a847da44e86bd6
Liam Hess <liamskydamien@gmail.com>
2023-02-05 17:15:16 +0100

    Finishing Touch

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import styles from "./Footer.module.css";
-import {FaReact} from "react-icons/fa";
+import {FaEnvelope, FaReact} from "react-icons/fa";
 const Footer = () => {
     return <footer className={styles.footer}>
         <div>
             <p>This site was fully created using React <FaReact color={"#61DBFB"}/> <br/>
                 Since its my first site created with React please dont judge to hard :)
+            </p>
+            <p>
+                Feel free to contact my any time at <a href="mailto:liamskydamien@gmail.com" className={styles.email}> <FaEnvelope/> liamskydamien@gmail.com </a>
             </p>
             <p>© 2021 - All Rights Reserved</p>
         </div>
